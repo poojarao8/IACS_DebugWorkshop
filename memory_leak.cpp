bfa2c41a1ebbf70b7c81059bcb885061f44c3c7f
poojarao8 <poojarao8@users.noreply.github.com>
2019-04-13 12:17:11 -0500

    Memory leak demo

---
+++ memory_leak.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h> 
+
+#define ARR_SIZE 256
+
+using namespace std; 
+  
+int main() 
+{ 
+  
+  double *R1= new double[ARR_SIZE];
+  double *R2= new double[ARR_SIZE];
+  double *R3= new double[ARR_SIZE];
+  double *R4= new double[ARR_SIZE];
+
+  for (int i=0; i<ARR_SIZE; ++i)
+  {
+    R1[i] = 1.0*i;
+    R2[i] = 2.0*i;
+    R3[i] = 3.0*i;
+    R4[i] = 4.0*i;
+  }
+ 
+  
+  return 0; 
+} 
+
